2ef37d6661a63906014a4860a88e52a4e57b88bf
LazyDotExe <papaporo@proton.me>
2023-08-26 13:51:23 -0500

    added material ui and a card option to diplsay movies.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 import "./MovieList.css";
 
 function MovieList() {
+    
   const dispatch = useDispatch();
   const movies = useSelector((store) => store.movies);
   const history = useHistory();
@@ -35,14 +42,24 @@ function MovieList() {
       <section className="movies">
         {movies.map((movie) => {
           return (
+            <Box sx={{ minWidth: 275 }}>
+            <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             <div key={movie.id}>
+                
               <h3>{movie.title}</h3>
+              
               <img
                 onClick={() => showDetails(movie)}
                 src={movie.poster}
                 alt={movie.title}
               />
+              
             </div>
+            
+            </Typography>
+            </CardContent>
+            </Box>
           );
         })}
       </section>
